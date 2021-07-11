d8815ee656d6b20db7bfad36c98bad549d1e054c
Belgin Vinoj <belgin.vinoj@iopex.com>
2021-07-11 11:47:45 +0530

    init

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# Create your views here.
+
+class Sample_Class(APIView):
+    def post(self,request):
+        return Response('post method called')
